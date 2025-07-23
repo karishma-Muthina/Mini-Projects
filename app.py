efec1f129d9071871f1359f3c18a2bc86596ae2e
karishma-Muthina <143176969+karishma-Muthina@users.noreply.github.com>
2025-07-23 16:22:39 +0530

    app.py

--- app.py
+++ app.py
@@ -53,20 +53,3 @@ plt.show()
 
 # Display summary
 category_summary.reset_index().head(10)
-
-Columns in the dataset: Index(['App', 'Category', 'Rating', 'Reviews', 'Size', 'Installs', 'Type',
-       'Price', 'Content Rating', 'Genres', 'Last Updated', 'Current Ver',
-       'Android Ver'],
-      dtype='object')
-
-Category	Installs	Rating	Reviews
-0	COMMUNICATION	4.311813e+07	4.121484	1.116449e+06
-1	SOCIAL	2.703370e+07	4.247291	1.122795e+06
-2	VIDEO_PLAYERS	2.653241e+07	4.044595	4.559735e+05
-3	ENTERTAINMENT	2.072216e+07	4.135294	3.408103e+05
-4	PRODUCTIVITY	1.924608e+07	4.183389	1.846857e+05
-5	PHOTOGRAPHY	1.767735e+07	4.157414	4.005750e+05
-6	TRAVEL_AND_LOCAL	1.548053e+07	4.069519	1.434203e+05
-7	GAME	1.521794e+07	4.247368	6.823420e+05
-8	NEWS_AND_MAGAZINES	1.161329e+07	4.121569	1.133787e+05
-9	TOOLS	1.114168e+07	4.039554	3.194369e+05
